ea3967acd95dfca704144055d7d67d706d4ef306
Joshua McCord <joshuacmccord@gmail.com>
2021-03-12 21:27:40 -0500

    AlamoFire Req to Trefle is grabbing Plant

--- PlantManager/ContentView.swift
+++ PlantManager/ContentView.swift
@@ -15,11 +15,5 @@ struct ContentView: View {
 }
 
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
 
 
--- PlantManager/ViewModels/PlantSearchViewModel.swift
+++ PlantManager/ViewModels/PlantSearchViewModel.swift
@@ -6,28 +6,44 @@
 //
 
 import Foundation
+import Alamofire
 //import TrefleSwiftSDK
 
 
 class PlantSearchViewModel : ObservableObject {
     
-    func getJson() -> String {
-        let urlString = "https://trefle.io/api/v1/plants/?token=_SsZd0R46iCZ5QF9zHP9eyeONarcvEvU5CsO-fOfRIg"
-        if let url = URL(string: urlString)
-        {
-            URLSession.shared.dataTask(with: url) { data, res, err in
-                if let data = data {
-                    print("hey")
-                    print("JSON String: \(String(data: data, encoding: .utf8))")
-                    
-                    let decoder = JSONDecoder()
-                    if let json = try? decoder.decode(Plant.self, from: data) {
-                        print(json)
+    init () {
+        
+    }
+    
+    func addPlant(plant: String)  {
+        let par = ["q" : plant]
+        
+        AF.request("https://trefle.io/api/v1/plants/search?token=_SsZd0R46iCZ5QF9zHP9eyeONarcvEvU5CsO-fOfRIg",
+                   method: .get,
+                   parameters: par,
+                   encoder: URLEncodedFormParameterEncoder.default)
+            .cURLDescription { description in
+                //print(description)
+            }.responseJSON { response in
+                print(response)
+                //to get status code
+                if let status = response.response?.statusCode {
+                    switch(status){
+                        case 200:
+                            print("example success")
+                        default:
+                            print("Trefle error with response status: \(status)")
                     }
                 }
-            }.resume()
-        }
-        return "test"
+                do {
+                    // parse json reponse here
+                    print("Trefle Success")
+                } catch {
+                    print("json error")
+                }
+                
+            }
     }
     
 }
