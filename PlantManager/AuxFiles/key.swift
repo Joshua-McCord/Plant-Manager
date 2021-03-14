ebc714292de3b9eded1ccd2e2661d0751c36c708
Joshua McCord <joshuacmccord@gmail.com>
2021-03-13 23:21:15 -0500

    removed api key from gitignore

---
+++ PlantManager/AuxFiles/key.swift
@@ -0,0 +1,14 @@
+//
+//  key.swift
+//  PlantManager
+//
+//  Created by Joshua Cole McCord on 2/8/21.
+//
+
+import Foundation
+
+struct key {
+    let key:String = "AIzaSyC2qU07aJQr_ynLajWCeMdSWKNjrQg_9wQ"
+    
+    func getKey() -> String {return key}
+}
